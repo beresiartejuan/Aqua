80ae79397d87329093f2a123c768225306b3e971
Juan Beresiarte <juanberesiarte@gmail.com>
2023-12-25 22:10:10 -0300

    Add phpunit

---
+++ src/Validate.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Beresiartejuan\Aqua;
+
+class Validate
+{
+}
---
+++ tests/ValidateTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+final class ValidateTest extends TestCase
+{
+    public function testHolaMundo()
+    {
+        $this->assertTrue(true);
+    }
+}
